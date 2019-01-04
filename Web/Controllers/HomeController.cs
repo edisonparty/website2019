aa62636ffa6ce31f16a5947eeb169250e30613f7
Martin Evald <martin.evald@gmail.com>
2019-01-04 23:11:20 +0100

    New design for 2019; started adapting backend to accomodate for changes in frontend logic

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -1,17 +1,29 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+
+using Web.Models;
+using Web.Repositories;
 
 namespace Web.Controllers
 {
     public class HomeController : Controller
     {
+        private const string partitionKey = "edisonparty2019";
+		private IParticipantRepository participantRepository { get; }
+
+		public HomeController(IParticipantRepository participantRepository)
+		{
+			this.participantRepository = participantRepository;
+		}
+
         public IActionResult Index()
         {
-            return View();
+            return View(participantRepository.GetParticipants(partitionKey));
         }
 
         public IActionResult Error()
